38647dbf994c35b291a76f7d9828ec81cc61700e
francescammauri <>
2019-10-29 17:35:17 +0100

    assignment 6

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
-function preload(){
-  // put preload code here
-}
+var canvas;
+var h1;
+var x = 400
+var y = 300
 
 function setup() {
-  // put setup code here
+h1 = createElement('h1',' red');
+h1.mouseClicked(changeColor);
 }
 
 function draw() {
-  // put drawing code here
+
+  h1.position(x,y);
+  x = x + random(-50, 50);
+}
+
+function changeColor() {
+//callback
+  h1.style('color:red');
+
 }
